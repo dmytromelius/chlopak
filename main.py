a96461a4923950f91308fd7e27c76a636a54a8fe
dmytromelius <dmytro.melius@gmail.com>
2026-09-12 18:50:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Czy mozesz byc moim chlopakiem")
+a = int(input("Jaki masz wzrost - "))
+if a < 180:
+    print("za niski")
+else:
+    b = int(input("jaki masz IQ - "))
+    if b < 140:
+        print("ew nie")
+    else:
+        c = int(input("ile cm - "))
+        if c < 20:
+            print("eww nie")
+        else:
+            print("mozesz byc moim chlopakiem")
